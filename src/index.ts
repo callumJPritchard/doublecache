e0168d35ff1e96d2eed7244afb0ae59486318439
Callum Pritchard <cjohnpritchard@gmail.com>
2022-02-22 22:12:04 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,41 +11,6 @@ export default {
     cacheify
 }
 
-
-async function testLimit(limit: number) {
-    const cache = new DiskCache(limit)
-
-    const toTest = 5_000
-
-    const start = Date.now()
-
-    const proms = []
-
-    for (let i = 0; i < toTest; i++) {
-        proms.push(cache.set(`key${i}`, {
-            value: `value${i}`,
-            created: Date.now(),
-            lastAccessed: Date.now()
-        }))
-    }
-
-    await Promise.all(proms)
-
-    const end = Date.now()
-
-    console.log(`${limit}: ${toTest} items in ${end - start}ms`)
-}
-
-async function testRange() {
-    const ranges = [1, 2, 3, 4, 8, 16, 32, 64, 128, 256, 512]
-
-    for (const range of ranges) {
-        await testLimit(range)
-    }
-}
-
-testRange()
-
 export {
     doublecache,
     cacheify
